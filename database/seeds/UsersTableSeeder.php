91039162917a61fad39fab4fa5fb516a2f4e2d26
samuel <samuelkyama102@gmail.com>
2019-03-12 19:51:44 +0300

    change pass for developers

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,14 +20,26 @@ class UsersTableSeeder extends Seeder
             'password'=>Hash::make('EggNet345&*'),
             'type'=>'1',
             'username'=>'skality',
+            'is_employee'=>'1',
+            'is_admin'=>'1',
             ]);
         \Illuminate\Support\Facades\DB::table('users')->insert([
-            'name'=>'oscar',
-            'email'=>'oscar@skality.com',
+            'name'=>'test_admin',
+            'email'=>'test_admin@skality.com',
             'password'=>Hash::make('123456'),
             'type'=>'1',
             'is_employee'=>'1',
-            'username'=>'oscar',
+            'is_admin'=>'1',
+            'username'=>'test_admin',
+            ]);
+        \Illuminate\Support\Facades\DB::table('users')->insert([
+            'name'=>'test_employee',
+            'email'=>'test_employee@skality.com',
+            'password'=>Hash::make('123456'),
+            'type'=>'1',
+            'is_employee'=>'1',
+            'is_admin'=>'0',
+            'username'=>'test_employee',
             ]);
 
         $users = factory(\App\User::class, 30)->create();
